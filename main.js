71af88504a294437d0c315ffa27f873971b58dd7
2Diego2 <diegoabelleyra74@gmail.com>
2025-08-06 21:56:46 -0300

    Primer demo

---
+++ main.js
@@ -0,0 +1,33 @@
+//Enlazar index.html con main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'src', 'renderer.js')
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'src', 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
